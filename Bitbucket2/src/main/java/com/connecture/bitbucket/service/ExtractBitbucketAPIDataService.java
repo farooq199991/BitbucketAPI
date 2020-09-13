77e9ae29eda09e63de5203c258330af3bce5ecc7
farooq199991 <farooqmoghal.m@gmail.com>
2020-09-13 14:09:34 +0530

    second comiite

--- Bitbucket2/src/main/java/com/connecture/bitbucket/service/ExtractBitbucketAPIDataService.java
+++ Bitbucket2/src/main/java/com/connecture/bitbucket/service/ExtractBitbucketAPIDataService.java
@@ -16,6 +16,12 @@ import com.connecture.bitbucket.rest.BitbucketRest;
 @Component
 public class ExtractBitbucketAPIDataService {
 	
+
+
+
+
+
+
 	@Autowired
 	private BitbucketServiceImpl bitbucketServiceImp;
 	
